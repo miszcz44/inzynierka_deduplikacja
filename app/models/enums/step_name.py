2e0a983f0120bce01ed0231524bd1fc99a4fabfb
miszcz44 <erykb412@wp.pl>
2024-10-13 23:06:43 +0200

    feat: Update workflow steps

--- app/models/enums/step_name.py
+++ app/models/enums/step_name.py
@@ -3,9 +3,8 @@ from enum import Enum
 
 class StepName(str, Enum):
     DATA_READING = "data_reading"
+    DATA_PREPROCESSING = "data_preprocessing"
     BLOCK_BUILDING = "block_building"
-    BLOCK_CLEANING = "block_cleaning"
-    COMPARISON_CLEANING = "comparison_cleaning"
-    ENTITY_MATCHING = "entity_matching"
-    ENTITY_CLUSTERING = "entity_clustering"
+    FIELD_AND_RECORD_COMPARISON = "field_and_record_comparison"
+    ClASSIFICATION = "classification"
     EVALUATION = "evaluation"
--- frontend/src/DataProcessingFlow.js
+++ frontend/src/DataProcessingFlow.js
@@ -5,12 +5,11 @@ import { useParams } from "react-router-dom";
 
 const initialNodes = [
   { id: '1', data: { label: 'Data Reading' }, position: { x: 100, y: 100 }, style: { padding: '10px', border: '1px solid #777', background: '#fff' } },
-  { id: '2', data: { label: 'Block Building' }, position: { x: 100, y: 200 }, style: { padding: '10px', border: '1px solid #777', background: '#fff' } },
-  { id: '3', data: { label: 'Block Cleaning (Opcjonalny)' }, position: { x: 100, y: 300 }, style: { padding: '10px', border: '1px solid #777', background: '#fff' } },
-  { id: '4', data: { label: 'Comparison Cleaning (Opcjonalny)' }, position: { x: 100, y: 400 }, style: { padding: '10px', border: '1px solid #777', background: '#fff' } },
-  { id: '5', data: { label: 'Entity Matching' }, position: { x: 100, y: 500 }, style: { padding: '10px', border: '1px solid #777', background: '#fff' } },
-  { id: '6', data: { label: 'Entity Clustering' }, position: { x: 100, y: 600 }, style: { padding: '10px', border: '1px solid #777', background: '#fff' } },
-  { id: '7', data: { label: 'Evaluation' }, position: { x: 100, y: 700 }, style: { padding: '10px', border: '1px solid #777', background: '#fff' } },
+  { id: '2', data: { label: 'Data Pre-processing' }, position: { x: 100, y: 200 }, style: { padding: '10px', border: '1px solid #777', background: '#fff' } },
+  { id: '3', data: { label: 'Block Building' }, position: { x: 100, y: 300 }, style: { padding: '10px', border: '1px solid #777', background: '#fff' } },
+  { id: '4', data: { label: 'Field and Record Comparison' }, position: { x: 100, y: 400 }, style: { padding: '10px', border: '1px solid #777', background: '#fff' } },
+  { id: '5', data: { label: 'Classification' }, position: { x: 100, y: 500 }, style: { padding: '10px', border: '1px solid #777', background: '#fff' } },
+  { id: '6', data: { label: 'Evaluation' }, position: { x: 100, y: 600 }, style: { padding: '10px', border: '1px solid #777', background: '#fff' } },
 ];
 
 const initialEdges = [
@@ -19,7 +18,6 @@ const initialEdges = [
   { id: 'e3-4', source: '3', target: '4' },
   { id: 'e4-5', source: '4', target: '5' },
   { id: 'e5-6', source: '5', target: '6' },
-  { id: 'e6-7', source: '6', target: '7' },
 ];
 
 const defaultViewport = { x: 0, y: 0, zoom: 1.5 };
